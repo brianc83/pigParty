cf4f4e00c8d291d9e473599d225501095b2a4df4
Brian Collins <collinsb40@gmail.com>
2024-02-10 16:53:49 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '',
+    server:
+    {
+        host: true,
+        open: true
+    },
     build:
     {
         outDir: "../dist",
