aeb0513fd153cf06b1c2f3bfd5dd150a37c2c0ce
Brian Collins <collinsb40@gmail.com>
2024-02-10 17:34:55 -0800

    .

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     root: './',
-    publicDir: "./public/",
-    base: './',
+    publicDir: "public/",
     server:
     {
         host: true,
